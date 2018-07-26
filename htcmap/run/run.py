b0d6d763af74a479a55831b96b467647f721e619
JoshKarpel <josh.karpel@gmail.com>
2018-07-26 08:59:21 -0500

    cleaned up run.py

--- htcmap/run/run.py
+++ htcmap/run/run.py
@@ -6,20 +6,28 @@ from pathlib import Path
 
 import cloudpickle
 
-print(f'Landed on execute node {socket.getfqdn()} ({socket.gethostbyname(socket.gethostname())}) at {datetime.datetime.utcnow()}.')
-print(f'Execute node operating system: {os.uname()}')
 
-dir_contents = ", ".join(str(x) for x in Path.cwd().iterdir())
-print(f'Local directory contents: {dir_contents}')
+def print_node_info():
+    print(f'Landed on execute node {socket.getfqdn()} ({socket.gethostbyname(socket.gethostname())}) at {datetime.datetime.utcnow()}.')
+    print(f'Execute node operating system: {os.uname()}')
 
-with open('fn.pkl', mode = 'rb') as file:
-    fn = cloudpickle.load(file)
+    dir_contents = ", ".join(str(x) for x in Path.cwd().iterdir())
+    print(f'Local directory contents: {dir_contents}')
 
-with open(f'{sys.argv[1]}.in', mode = 'rb') as file:
-    args, kwargs = cloudpickle.load(file)
 
-output = fn(*args, **kwargs)
-print(output)
+def run_func(arg_hash):
+    with open('fn.pkl', mode = 'rb') as file:
+        fn = cloudpickle.load(file)
 
-with open(f'{sys.argv[1]}.out', mode = 'wb') as file:
-    cloudpickle.dump(output, file)
+    with open(f'{arg_hash}.in', mode = 'rb') as file:
+        args, kwargs = cloudpickle.load(file)
+
+    output = fn(*args, **kwargs)
+
+    with open(f'{arg_hash}.out', mode = 'wb') as file:
+        cloudpickle.dump(output, file)
+
+
+def main():
+    print_node_info()
+    run_func(arg_hash = sys.argv[1])
--- test.py
+++ test.py
@@ -8,7 +8,7 @@ def double(x):
 
 
 print(double)
-inputs = list((i,) for i in range(int(sys.argv[1]), int(sys.argv[2])))
+inputs = range(int(sys.argv[1]), int(sys.argv[2]))
 j = double.map(inputs)
 print(j)
 for i, result in zip(inputs, j):
