aac8d500f55f8b38161fd3feaac60088f32e75d0
JoshKarpel <josh.karpel@gmail.com>
2018-08-07 08:24:06 -0500

    export status

--- htmap/__init__.py
+++ htmap/__init__.py
@@ -1,5 +1,5 @@
 from .settings import settings
 from .mapper import htmap, HTMapper, MapResult, MapBuilder
 from .shortcuts import map, productmap, starmap, build_map, recover
-from .management import clean, map_ids
+from .management import clean, map_ids, status
 from . import exceptions
