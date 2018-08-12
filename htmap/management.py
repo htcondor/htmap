d6dee2fadc25dc42db43d6ec5f4e5670391161bc
JoshKarpel <karpel@wisc.edu>
2018-08-11 21:50:39 -0500

    better htmap.clean()

--- htmap/management.py
+++ htmap/management.py
@@ -11,21 +11,25 @@ def _map_paths() -> Iterator[Path]:
     yield from (settings.HTMAP_DIR / settings.MAPS_DIR_NAME).iterdir()
 
 
-def clean():
-    """Remove all input and output files for all existing maps."""
-    for dir in _map_paths():
-        shutil.rmtree(dir)
-
-
 def map_ids() -> Tuple[str]:
     """Return a tuple containing the ``map_id`` for existing maps."""
     return tuple(path.stem for path in _map_paths())
 
 
+def map_results() -> Tuple[mapper.MapResult]:
+    return tuple(shortcuts.recover(map_id) for map_id in map_ids())
+
+
+def clean():
+    """Remove all input and output files for all existing maps."""
+    for map_result in map_results():
+        map_result.remove()
+
+
 def status() -> str:
     """Return a string containing a table showing the status of all existing maps."""
     ids = map_ids()
-    results = [shortcuts.recover(map_id) for map_id in ids]
+    results = map_results()
     counts = [result._status_counts() for result in results]
 
     return utils.table(
