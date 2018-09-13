761efd59a4922da081c8d24a5f9465897d94ad2d
JoshKarpel <karpel@wisc.edu>
2018-09-13 13:15:42 -0500

    add license notices to source files

--- tests/test_recover_result.py
+++ tests/test_recover_result.py
@@ -1,3 +1,20 @@
+"""
+Copyright 2018 HTCondor Team, Computer Sciences Department,
+University of Wisconsin-Madison, WI.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 import time
 
 import pytest
@@ -36,4 +53,3 @@ def test_recover_classmethod(mapped_doubler):
 def test_recover_on_bad_mapid_raises_map_id_not_found():
     with pytest.raises(htmap.exceptions.MapIdNotFound):
         htmap.recover('no_such_mapid')
-
