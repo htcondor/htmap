f29106060c336c9242153aa71bdd97ea926b454e
JoshKarpel <karpel@wisc.edu>
2018-09-20 09:36:38 -0500

    must not use pip cache dir with docker python image

--- htmap/run/run.sh
+++ htmap/run/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-pip install --user --trusted-host pypi.org --trusted-host files.pythonhosted.org cloudpickle
+pip install --no-cache-dir --trusted-host pypi.org --trusted-host files.pythonhosted.org cloudpickle
 
 python run.py $1
