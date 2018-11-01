ea052d90744a8333015e1255563c126cfa72a7ea
JoshKarpel <karpel@wisc.edu>
2018-11-01 10:23:29 -0500

    list ids, clean, and hold reasons

--- htmap/cli.py
+++ htmap/cli.py
@@ -12,14 +12,27 @@ def cli():
 
 
 @cli.command()
+def ids():
+    """Print map ids."""
+    click.echo('\n'.join(htmap.map_ids()))
+
+
+@cli.command()
 def status():
     """Print the status of maps."""
     click.echo(htmap.status())
 
 
 @cli.command()
+def clean():
+    """Remove all maps."""
+    htmap.clean()
+
+
+@cli.command()
 @click.argument('id')
 def wait(id):
+    """Wait for a map to complete."""
     cli_load(id).wait(show_progress_bar = True)
 
 
@@ -44,9 +57,16 @@ def hold(id):
     cli_load(id).hold()
 
 
+@cli.command()
+@click.argument('id')
+def reasons(id):
+    """Print the hold reasons for a map."""
+    click.echo(cli_load(id).hold_reasons())
+
+
 def cli_load(map_id) -> htmap.Map:
     try:
         return htmap.load(map_id)
-    except:
-        click.echo(f'ERR: could not find map with map_id {map_id}')
+    except Exception as e:
+        click.echo(f'ERR: could not find a map with map_id {map_id}')
         sys.exit(1)
