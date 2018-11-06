5205df04c1cb4b55d712428dd889125366c9eecf
JoshKarpel <karpel@wisc.edu>
2018-11-06 08:21:22 -0600

    clean up CLI, add verbose-mode flag

--- htmap/cli.py
+++ htmap/cli.py
@@ -1,25 +1,46 @@
+import logging
 import sys
 
 import htmap
 
 import click
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
-@click.group()
-def cli():
+CONTEXT_SETTINGS = dict(help_option_names = ['-h', '--help'])
+
+
+@click.group(context_settings = CONTEXT_SETTINGS)
+@click.option('--verbose', '-v', is_flag = True, default = False, help = 'Show log messages as the CLI runs.')
+def cli(verbose):
     """HTMap command line tools."""
-    pass
+    if verbose:
+        _start_htmap_logger()
+
+
+def _start_htmap_logger():
+    htmap_logger = logging.getLogger('htmap')
+    htmap_logger.setLevel(logging.DEBUG)
+
+    handler = logging.StreamHandler(stream = sys.stdout)
+    handler.setLevel(logging.DEBUG)
+    handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+
+    htmap_logger.addHandler(handler)
+
+    return handler
 
 
 @cli.command()
 def ids():
     """Print map ids."""
-    click.echo(id_list())
+    click.echo(_id_list())
 
 
 @cli.command()
 def status():
-    """Print the status of maps."""
+    """Print the status of all maps."""
     click.echo(htmap.status())
 
 
@@ -30,45 +51,51 @@ def clean():
 
 
 @cli.command()
-@click.argument('id')
-def wait(id):
-    """Wait for a map to complete."""
-    cli_load(id).wait(show_progress_bar = True)
+@click.argument('ids', nargs = -1)
+def wait(ids):
+    """Wait for maps to complete."""
+    for map_id in ids:
+        _cli_load(map_id).wait(show_progress_bar = True)
 
 
 @cli.command()
-@click.argument('id')
-def remove(id):
-    """Remove a map."""
-    cli_load(id).remove()
+@click.argument('ids', nargs = -1)
+def remove(ids):
+    """Remove maps."""
+    for map_id in ids:
+        _cli_load(map_id).remove()
 
 
 @cli.command()
-@click.argument('id')
-def release(id):
-    """Release a map."""
-    cli_load(id).release()
+@click.argument('ids', nargs = -1)
+def release(ids):
+    """Release maps."""
+    for map_id in ids:
+        _cli_load(map_id).release()
 
 
 @cli.command()
-@click.argument('id')
-def hold(id):
-    """Hold a map."""
-    cli_load(id).hold()
+@click.argument('ids', nargs = -1)
+def hold(ids):
+    """Hold maps."""
+    for map_id in ids:
+        _cli_load(map_id).hold()
 
 
 @cli.command()
-@click.argument('id')
-def reasons(id):
-    """Print the hold reasons for a map."""
-    click.echo(cli_load(id).hold_report())
+@click.argument('ids', nargs = -1)
+def reasons(ids):
+    """Print the hold reasons for maps."""
+    for map_id in ids:
+        click.echo(_cli_load(map_id).hold_report())
 
 
 @cli.command()
 @click.argument('id')
 @click.argument('newid')
 def rename(id, newid):
-    cli_load(id).rename(newid)
+    """Rename a map."""
+    _cli_load(id).rename(newid)
 
 
 @cli.command()
@@ -77,16 +104,16 @@ def version():
     click.echo(htmap.version())
 
 
-def cli_load(map_id) -> htmap.Map:
+def _cli_load(map_id) -> htmap.Map:
     try:
         return htmap.load(map_id)
     except Exception as e:
         click.echo(f'Error: could not find a map with map_id {map_id}')
         click.echo(f'Your map ids are...')
-        click.echo(id_list())
+        click.echo(_id_list())
 
         sys.exit(1)
 
 
-def id_list() -> str:
+def _id_list() -> str:
     return '\n'.join(htmap.map_ids())
