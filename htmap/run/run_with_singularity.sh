8f2c4d789038834c6fc112d43bed79c1cece8b78
JoshKarpel <karpel@wisc.edu>
2019-04-02 15:57:07 -0500

    must set singularity cache, maybe?

--- htmap/run/run_with_singularity.sh
+++ htmap/run/run_with_singularity.sh
@@ -5,4 +5,6 @@ set -e
 img=$1
 component=$2
 
+export SINGULARITY_CACHEDIR=$_CONDOR_SCRATCH_DIR
+
 singularity exec --bind ${_CONDOR_SCRATCH_DIR}:/htmap_scratch ${img} /htmap_scratch/run.py ${component}
