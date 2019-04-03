53e87b074c793f86e8f75e4b0216dfaee4104ab8
JoshKarpel <karpel@wisc.edu>
2019-04-03 13:51:37 -0500

    smoke test

--- htmap/run/run_with_singularity.sh
+++ htmap/run/run_with_singularity.sh
@@ -5,6 +5,7 @@ set -e
 img=$1
 component=$2
 
+# would otherwise default to user home dir
 export SINGULARITY_CACHEDIR=$_CONDOR_SCRATCH_DIR
 
-singularity exec --bind ${_CONDOR_SCRATCH_DIR}:/scratch/htmap ${img} bash -c "ls; ls /scratch; ls /scratch/htmap; cd /scratch/htmap; ./run.py ${component}"
+singularity exec --bind ${_CONDOR_SCRATCH_DIR}:/htmap/scratch ${img} bash -c "cd /htmap/scratch; ls"
