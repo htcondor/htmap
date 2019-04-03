810c43e79ef8027333b50a9d292b4a048ffe2b00
JoshKarpel <karpel@wisc.edu>
2019-04-03 10:47:15 -0500

    back to basics...

--- htmap/run/run_with_singularity.sh
+++ htmap/run/run_with_singularity.sh
@@ -7,4 +7,4 @@ component=$2
 
 export SINGULARITY_CACHEDIR=$_CONDOR_SCRATCH_DIR
 
-singularity exec --writable --bind ${_CONDOR_SCRATCH_DIR}:/htmap_scratch ${img} bash -c "mkdir /htmap_scratch; ls /htmap_scratch; python3 /htmap_scratch/run.py ${component}"
+singularity exec --bind ${_CONDOR_SCRATCH_DIR}:/_htmap_scratch ${img} python3 /_htmap_scratch/run.py ${component}
