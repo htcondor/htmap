42a9dc5148aa9348961b5061d7217f23ecf7f759
JoshKarpel <karpel@wisc.edu>
2019-04-03 14:02:09 -0500

    more smoke...

--- htmap/run/run_with_singularity.sh
+++ htmap/run/run_with_singularity.sh
@@ -6,6 +6,12 @@ img=$1
 component=$2
 
 # would otherwise default to user home dir
-export SINGULARITY_CACHEDIR=$_CONDOR_SCRATCH_DIR
+export SINGULARITY_CACHEDIR=${_CONDOR_SCRATCH_DIR}
 
-singularity exec --bind ${_CONDOR_SCRATCH_DIR}:/htmap/scratch ${img} bash -c "cd /htmap/scratch; ls"
+echo outside
+pwd
+ls -l
+echo img ${img}
+echo component ${component}
+
+singularity exec --bind ${_CONDOR_SCRATCH_DIR}:/htmap/scratch ${img} bash -c "echo inside; pwd; cd /htmap/scratch; pwd; ls -l; ls -l /"
