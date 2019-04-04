d181cbc66cd5f19e57ffc33394819efb97c8c822
JoshKarpel <karpel@wisc.edu>
2019-04-04 14:34:43 -0500

    use && instead of ;

--- htmap/run/run_with_singularity.sh
+++ htmap/run/run_with_singularity.sh
@@ -8,4 +8,4 @@ component=$2
 # would otherwise default to user home dir
 export SINGULARITY_CACHEDIR=${_CONDOR_SCRATCH_DIR}
 
-singularity exec --bind ${_CONDOR_SCRATCH_DIR}:/htmap/scratch ${img} bash -c "cd /htmap/scratch; python3 run.py ${component}"
+singularity exec --bind ${_CONDOR_SCRATCH_DIR}:/htmap/scratch ${img} bash -c "cd /htmap/scratch && python3 run.py ${component}"
