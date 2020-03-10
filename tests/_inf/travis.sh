133b2a43e6316c1c89323a42715f9561022ea9f3
Josh Karpel <karpel@wisc.edu>
2020-03-10 12:38:49 -0500

    coverage pipeline should go through /tmp

--- tests/_inf/travis.sh
+++ tests/_inf/travis.sh
@@ -2,7 +2,11 @@
 
 set -e
 
-echo condor_version
-pytest -n 2 --cov
+python -c "import htcondor; print(htcondor.version())"
+pytest --version
 
-codecov -t 492519e2-1bcf-4e8a-8a3e-e28be5d9de8d
+pytest -n 2 --cov --durations=20
+
+coverage xml -o /tmp/coverage.xml
+
+codecov -t 492519e2-1bcf-4e8a-8a3e-e28be5d9de8d -f /tmp/coverage.xml
