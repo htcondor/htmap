f65066d175f056a9c7bb4efe019c3668f9451c0f
Josh Karpel <karpel@wisc.edu>
2020-05-06 10:12:41 -0500

    will the improved diagnostics never end?

--- tests/travis.sh
+++ tests/travis.sh
@@ -18,4 +18,4 @@ pytest -n 4 --cov --durations=20
 
 coverage xml -o /tmp/coverage.xml
 
-codecov -t 492519e2-1bcf-4e8a-8a3e-e28be5d9de8d -f /tmp/coverage.xml
+codecov -X gcov -t 492519e2-1bcf-4e8a-8a3e-e28be5d9de8d -f /tmp/coverage.xml
