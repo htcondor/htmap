00da9d5ac170fdfbfc5a59f63dddf3c64be66fbc
Josh Karpel <josh.karpel@gmail.com>
2020-05-23 15:28:17 -0500

    put codecov token in codecov.yml instead of tests/ci.sh

--- tests/ci.sh
+++ tests/ci.sh
@@ -22,4 +22,4 @@ printf "\n-----\n"
 pytest -n 4 --cov --durations=20
 
 coverage xml -o /tmp/coverage.xml
-codecov -X gcov -t 492519e2-1bcf-4e8a-8a3e-e28be5d9de8d -f /tmp/coverage.xml
+codecov -X gcov -f /tmp/coverage.xml
